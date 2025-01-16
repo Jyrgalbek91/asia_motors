81d2d8dba27f0267e39637ec6dde050219704388
ALMAZBEKOV <jyrgalbekalmazbekov@gmail.com>
2025-01-16 11:05:35 +0600

    delete images and add storage route

--- back/src/routers/index.ts
+++ back/src/routers/index.ts
@@ -1,8 +1,12 @@
 import { Request, Response, Router } from "express";
 import userRoute from "../user";
 import PostRoute from "../post";
+import storageRoute from "../storage";
 
-const api = Router().use("/user", userRoute).use("/post", PostRoute);
+const api = Router()
+  .use("/user", userRoute)
+  .use("/post", PostRoute)
+  .use("/storage", storageRoute);
 
 api.use((_: Request, res: Response) => {
   res.status(404).json({ error: "API not found" });
